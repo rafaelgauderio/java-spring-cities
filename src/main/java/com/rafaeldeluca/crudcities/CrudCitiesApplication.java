66dd76b51a426a7fd7d479db3e3b253251922a31
Rafael de Luca <deluca1712@gmail.com>
2024-09-30 15:04:38 -0300

    create project

---
+++ src/main/java/com/rafaeldeluca/crudcities/CrudCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.crudcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaeldeluca/crudcities/CrudCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.crudcities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
